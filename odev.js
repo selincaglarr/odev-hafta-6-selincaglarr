c220447ca799cb23ae948fbd3f995a8c70ff2ce8
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2021-01-06 00:04:54 +0300

    Update odev.js

--- odev.js
+++ odev.js
@@ -2,8 +2,19 @@ window.mockApiUrl = "https://5ff193d0db1158001748b15e.mockapi.io/pets/";
 
 window.removePet = (id) => {
     console.log(id) // remove the pet with given id
+    fetch('${window.mockApiUrl}${id}',{
+        method:"DELETE",
+        headers:{
+            'Accept':'application/json , text/plain , */*',
+            'Content-Type':'application/json'
+        }
+    
+    
+    }).then( () => {
+        window.location.reload(); // basarili olursa sayfayi yenile diyoruz
+    });
+  
 };
-
 window.openPetDetail = (id) => {
     console.log(id); // show details of the pet with given id
 };
