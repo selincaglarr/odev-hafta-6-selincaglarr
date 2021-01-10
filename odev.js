a1ce6921d4a54e9daf268cf830de4b714799d072
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2021-01-10 10:36:01 +0300

    Update odev.js

--- odev.js
+++ odev.js
@@ -1,20 +1,51 @@
-window.mockApiUrl = "https://5ff193d0db1158001748b15e.mockapi.io/pets/";
+window.mockApiUrl = "https://5ff1a6a2db1158001748b2ab.mockapi.io/pets/";
 
 window.removePet = (id) => {
-    console.log(id) // remove the pet with given id
-    fetch('${window.mockApiUrl}${id}',{
-        method:"DELETE",
-        headers:{
-            'Accept':'application/json , text/plain , */*',
-            'Content-Type':'application/json'
+    fetch(`${window.mockApiUrl}${id}`, {
+        method: "DELETE",
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
         }
-    
-    
-    }).then( () => {
-        window.location.reload(); // basarili olursa sayfayi yenile diyoruz
-    });
-  
+
+
+    })
+    .then(()=> {
+        window.location.reload();
+    })
 };
+
+window.generateDetailModal = (pet) => {
+
+    return ` <div class="modal fade" id="exampleModal${pet.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ${pet.name}
+          <p>${pet.description}</p>
+        </div>
+        <div class = "modal-footer>
+          <button type="button" class ="btn btn-success" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>`
+
+}
+
 window.openPetDetail = (id) => {
-    console.log(id); // show details of the pet with given id
+    fetch(`${window.mockApiUrl}${id}`)
+    .then(response => response.json())
+    .then((data) => {
+      const petModalHTMl =  generateDetailModal(data);
+      document.querySelector("body").innerHTML += petModalHTMl;
+      $(`#exampleModal${id}`).modal('show');
+      document.querySelector(`.exampleModal${id}`)
+    })
+
+
+
 };
