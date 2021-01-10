c55baa3604c2eb8543c81ce26756aeab5abfaf95
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2021-01-10 12:55:19 +0300

    Update odev.js

--- odev.js
+++ odev.js
@@ -1,51 +1,43 @@
-window.mockApiUrl = "https://5ffaafa487478d0017d9a780.mockapi.io/pets";
 
+
+window.mockApiUrl = "https://5ffacd6c87478d0017d9a895.mockapi.io/pets";
 window.removePet = (id) => {
-    fetch(`${window.mockApiUrl}${id}`, {
-        method: "DELETE",
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        }
-
-
-    })
-    .then(()=> {
-        window.location.reload();
-    })
+  fetch(`${window.mockApiUrl}${id}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+  }).then(() => {
+    window.location.reload();
+  });
 };
 
 window.generateDetailModal = (pet) => {
-
-    return ` <div class="modal fade" id="exampleModal${pet.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+  const petModalHTMl = ` <div class="modal fade" id="exampleModal${pet.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+          <h5 class="modal-title" id="exampleModalLabel">${pet.name}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
         <div class="modal-body">
-          ${pet.name}
-          <p>${pet.description}</p>
+        <img src=${pet.image} class="card-img-top">
+          ${pet.description}
         </div>
-        <div class = "modal-footer>
-          <button type="button" class ="btn btn-success" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
-  </div>`
-
-}
+  </div>`;
+  document.querySelector("body").innerHTML += petModalHTMl;
+};
 
 window.openPetDetail = (id) => {
-    fetch(`${window.mockApiUrl}${id}`)
-    .then(response => response.json())
+  fetch(`${window.mockApiUrl}${id}`)
+    .then((response) => response.json())
     .then((data) => {
-      const petModalHTMl =  generateDetailModal(data);
-      document.querySelector("body").innerHTML += petModalHTMl;
-      $(`#exampleModal${id}`).modal('show');
-      document.querySelector(`.exampleModal${id}`)
-    })
-
-
-
+      generateDetailModal(data);
+      $(`#exampleModal${id}`).modal("show");
+    });
 };
